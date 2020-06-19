3cd57f9fa0d2aabae3db5e41858dd2ef4b545ce1
Matijs van Zuijlen <matijs@matijs.net>
2020-06-19 19:09:37 +0200

    Add test and override for Gtk::Dialog from gir_ffi-gtk

--- lib/gir_ffi-gtk4.rb
+++ lib/gir_ffi-gtk4.rb
@@ -4,6 +4,7 @@ require "gir_ffi"
 
 GirFFI.setup :Gtk, "4.0"
 
+require "gir_ffi-gtk4/dialog"
 require "gir_ffi-gtk4/file_chooser_dialog"
 require "gir_ffi-gtk4/list_store"
 require "gir_ffi-gtk4/message_dialog"
---
+++ test/gir_ffi-gtk4/dialog_test.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+describe Gtk::Dialog do
+  describe ".new_with_buttons" do
+    it "creates a Gtk::Dialog with the right title and buttons" do
+      dialog = Gtk::Dialog.new_with_buttons "Foo", nil, :modal, [["Bar", :yes]]
+      _(dialog.title).must_equal "Foo"
+      button = dialog.widget_for_response :yes
+      _(button.label).must_equal "Bar"
+      _(dialog.response_for_widget(button)).must_equal Gtk::ResponseType.to_int(:yes)
+    end
+  end
+end
