204d7f4f25029719c724442af3c410f206c3f0ec
Matijs van Zuijlen <matijs@matijs.net>
2021-11-27 11:35:50 +0100

    Update test code to fetch button from FileChooserDialog
    In GTK+ 4, the FileChooserDialog has a different structure, so we need
    some slightly more involved code to find the custom button.

--- test/gir_ffi-gtk4/file_chooser_dialog_test.rb
+++ test/gir_ffi-gtk4/file_chooser_dialog_test.rb
@@ -8,11 +8,15 @@ describe Gtk::FileChooserDialog do
       dialog = Gtk::FileChooserDialog.new "Foo", nil, :save, [["Bar", :yes]]
       _(dialog.title).must_equal "Foo"
       _(dialog.action).must_equal :save
-      button = if dialog.use_header_bar == 1
-                 dialog.header_bar.children.to_a.last
-               else
-                 dialog.action_area.children.to_a.last
-               end
+
+      # Check assumption of following code
+      _(dialog.use_header_bar).must_equal 1
+      header_bar = dialog.header_bar
+      window_handle = header_bar.first_child
+      center_box = window_handle.child
+      button_box = center_box.end_widget
+      button = button_box.first_child
+
       _(button.label).must_equal "Bar"
       _(dialog.response_for_widget(button)).must_equal Gtk::ResponseType.to_int(:yes)
     end
